194f00c9be4786a35aed6777ee4e26180dcc51e1
pwnzor1337 <kaleb.mcclenathan@gmail.com>
2018-01-19 21:00:36 -0600

    Waht.

---
+++ app/src/main/java/ftc/scoutingapp/AddScoringActivity.java
@@ -0,0 +1,17 @@
+package ftc.scoutingapp;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kaleb on 1/19/2018.
+ */
+
+public class AddScoringActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
